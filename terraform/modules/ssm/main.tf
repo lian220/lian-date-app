48c50e0d638ae06df6649eabd2efbc852663c4e3
lian220 <lian.dy220@gmail.com>
2026-01-29 19:57:22 +0900

    fix(terraform): SSM Parameter deprecated overwrite 인자 제거
    - aws_ssm_parameter의 deprecated overwrite=true 제거
    - 기존 lifecycle { ignore_changes = [value] } 블록으로 이미 처리됨
    - Terraform 버전 업그레이드 시 호환성 유지

--- terraform/modules/ssm/main.tf
+++ terraform/modules/ssm/main.tf
@@ -13,7 +13,6 @@ resource "aws_ssm_parameter" "db_host" {
   type        = "SecureString"
   value       = var.db_host
   tier        = "Standard"
-  overwrite   = true
 
   tags = merge(var.tags, {
     Name = "${var.project_name}-${var.environment}-db-host"
@@ -30,7 +29,6 @@ resource "aws_ssm_parameter" "db_port" {
   type        = "String"
   value       = var.db_port
   tier        = "Standard"
-  overwrite   = true
 
   tags = merge(var.tags, {
     Name = "${var.project_name}-${var.environment}-db-port"
@@ -47,7 +45,6 @@ resource "aws_ssm_parameter" "db_name" {
   type        = "String"
   value       = var.db_name
   tier        = "Standard"
-  overwrite   = true
 
   tags = merge(var.tags, {
     Name = "${var.project_name}-${var.environment}-db-name"
@@ -64,7 +61,6 @@ resource "aws_ssm_parameter" "db_username" {
   type        = "SecureString"
   value       = var.db_username
   tier        = "Standard"
-  overwrite   = true
 
   tags = merge(var.tags, {
     Name = "${var.project_name}-${var.environment}-db-username"
@@ -81,7 +77,6 @@ resource "aws_ssm_parameter" "db_password" {
   type        = "SecureString"
   value       = var.db_password
   tier        = "Standard"
-  overwrite   = true
 
   tags = merge(var.tags, {
     Name = "${var.project_name}-${var.environment}-db-password"
@@ -101,7 +96,6 @@ resource "aws_ssm_parameter" "openai_api_key" {
   type        = "SecureString"
   value       = var.openai_api_key
   tier        = "Standard"
-  overwrite   = true
 
   tags = merge(var.tags, {
     Name = "${var.project_name}-${var.environment}-openai-api-key"
@@ -118,7 +112,6 @@ resource "aws_ssm_parameter" "kakao_rest_api_key" {
   type        = "SecureString"
   value       = var.kakao_rest_api_key
   tier        = "Standard"
-  overwrite   = true
 
   tags = merge(var.tags, {
     Name = "${var.project_name}-${var.environment}-kakao-rest-api-key"
@@ -135,7 +128,6 @@ resource "aws_ssm_parameter" "kakao_javascript_key" {
   type        = "SecureString"
   value       = var.kakao_javascript_key
   tier        = "Standard"
-  overwrite   = true
 
   tags = merge(var.tags, {
     Name = "${var.project_name}-${var.environment}-kakao-javascript-key"
